f53f52751230c7048d1fa8bf6b96baeaefc450c1
Admas Terefe Girma <admasterefe00@gmail.com>
2025-03-31 18:20:47 +0300

    interaction-1

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="CryptoCLI",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points="""
+        [console_scripts]
+        CryptoCLI=CryptoCLI.main:cli
+    """,
+    author="Admas Terefe Girma",
+    author_email="aadmasterefe00@gmail.com",
+    description="A command-line tool for viewing cryptocurrency statistics from CoinGecko",
+)
